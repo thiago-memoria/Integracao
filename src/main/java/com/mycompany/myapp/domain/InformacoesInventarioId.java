61aa351086d0e9edd1656edfa5918b11ecb295b9
fhumbertodf <fhumbertodf@gmail.com>
2022-12-11 21:56:30 -0300

    Informacoes Inventario

---
+++ src/main/java/com/mycompany/myapp/domain/InformacoesInventarioId.java
@@ -0,0 +1,73 @@
+package com.mycompany.myapp.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * A InformacoesInventarioId.
+ */
+@Embeddablepublic class InformacoesInventarioId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    private ParametroFuncionaisCentroCusto parametroFuncionaisCentroCusto;
+
+    @ManyToOne
+    private Itens itens;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public ParametroFuncionaisCentroCusto getParametroFuncionaisCentroCusto() {
+        return this.parametroFuncionaisCentroCusto;
+    }
+
+    public void setParametroFuncionaisCentroCusto(ParametroFuncionaisCentroCusto parametroFuncionaisCentroCusto) {
+        this.parametroFuncionaisCentroCusto = parametroFuncionaisCentroCusto;
+    }
+
+    public InformacoesInventarioId parametroFuncionaisCentroCusto(ParametroFuncionaisCentroCusto parametroFuncionaisCentroCusto) {
+        this.setParametroFuncionaisCentroCusto(parametroFuncionaisCentroCusto);
+        return this;
+    }
+
+    public Itens getItens() {
+        return this.itens;
+    }
+
+    public void setItens(Itens itens) {
+        this.itens = itens;
+    }
+
+    public InformacoesInventarioId itens(Itens itens) {
+        this.setItens(itens);
+        return this;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof InformacoesInventarioId)) {
+            return false;
+        }
+        return id != null && id.equals(((InformacoesInventarioId) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "InformacoesInventarioId{" +
+            "id=" + getId() +
+            "}";
+    }
+}
---
+++ src/main/java/com/mycompany/myapp/service/impl/InformacoesInventarioServiceImpl.java
@@ -0,0 +1,96 @@
+package com.mycompany.myapp.service.impl;
+
+import com.mycompany.myapp.domain.InformacoesInventario;
+import com.mycompany.myapp.repository.InformacoesInventarioRepository;
+import com.mycompany.myapp.service.InformacoesInventarioService;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service Implementation for managing {@link InformacoesInventario}.
+ */
+@Service
+@Transactional
+public class InformacoesInventarioServiceImpl implements InformacoesInventarioService {
+
+    private final Logger log = LoggerFactory.getLogger(InformacoesInventarioServiceImpl.class);
+
+    private final InformacoesInventarioRepository informacoesInventarioRepository;
+
+    public InformacoesInventarioServiceImpl(InformacoesInventarioRepository informacoesInventarioRepository) {
+        this.informacoesInventarioRepository = informacoesInventarioRepository;
+    }
+
+    @Override
+    public InformacoesInventario save(InformacoesInventario informacoesInventario) {
+        log.debug("Request to save InformacoesInventario : {}", informacoesInventario);
+        return informacoesInventarioRepository.save(informacoesInventario);
+    }
+
+    @Override
+    public InformacoesInventario update(InformacoesInventario informacoesInventario) {
+        log.debug("Request to update InformacoesInventario : {}", informacoesInventario);
+        return informacoesInventarioRepository.save(informacoesInventario);
+    }
+
+    @Override
+    public Optional<InformacoesInventario> partialUpdate(InformacoesInventario informacoesInventario) {
+        log.debug("Request to partially update InformacoesInventario : {}", informacoesInventario);
+
+        return informacoesInventarioRepository
+            .findById(informacoesInventario.getId())
+            .map(existingInformacoesInventario -> {
+                if (informacoesInventario.getIte_bar() != null) {
+                    existingInformacoesInventario.setIte_bar(informacoesInventario.getIte_bar());
+                }
+                if (informacoesInventario.getEst_qan() != null) {
+                    existingInformacoesInventario.setEst_qan(informacoesInventario.getEst_qan());
+                }
+                if (informacoesInventario.getEst_qtd() != null) {
+                    existingInformacoesInventario.setEst_qtd(informacoesInventario.getEst_qtd());
+                }
+                if (informacoesInventario.getEst_pre() != null) {
+                    existingInformacoesInventario.setEst_pre(informacoesInventario.getEst_pre());
+                }
+                if (informacoesInventario.getEst_val() != null) {
+                    existingInformacoesInventario.setEst_val(informacoesInventario.getEst_val());
+                }
+                if (informacoesInventario.getEst_pag() != null) {
+                    existingInformacoesInventario.setEst_pag(informacoesInventario.getEst_pag());
+                }
+                if (informacoesInventario.getEst_dat() != null) {
+                    existingInformacoesInventario.setEst_dat(informacoesInventario.getEst_dat());
+                }
+                if (informacoesInventario.getEst_hor() != null) {
+                    existingInformacoesInventario.setEst_hor(informacoesInventario.getEst_hor());
+                }
+
+                return existingInformacoesInventario;
+            })
+            .map(informacoesInventarioRepository::save);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<InformacoesInventario> findAll() {
+        log.debug("Request to get all InformacoesInventarios");
+        return informacoesInventarioRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<InformacoesInventario> findOne(Long id) {
+        log.debug("Request to get InformacoesInventario : {}", id);
+        return informacoesInventarioRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete InformacoesInventario : {}", id);
+        informacoesInventarioRepository.deleteById(id);
+    }
+}
